e94e9fc5c1e69b7b4a86ef2b548f0544e938f893
Yi Lu <121090386@link.cuhk.edu.cn>
2025-12-28 18:50:32 -0500

    Add files via upload

---
+++ git_script/git_push_branch.sh
@@ -0,0 +1,9 @@
+# uncomment if it is in linux, and need to convert dos to unix
+#sed -i 's/\r$//' git_script/git_push.sh
+read -p "Enter the name of your feature branch: " BRANCH_NAME
+
+git add .
+
+git commit -m "update"
+
+git push origin "$BRANCH_NAME"
